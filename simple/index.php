097cf8404b8b95a07f6d00f18c9c49dcfbddb65d
Miyayu <yuchan2215@gmail.com>
2021-11-18 11:11:56 +0900

    mainでかこう

--- simple/index.php
+++ simple/index.php
@@ -5,9 +5,11 @@
     <title>Simple Page</title>
 </head>
 <body>
+<main>
 <form action="submit.php" method="post">
     <input type="text" name="comment" placeholder="何かコメントを入力">
     <input type="submit" value="送信">
 </form>
+</main>
 </body>
 </html>
